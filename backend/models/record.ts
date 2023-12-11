74009b5d5605953c02d9104cd37d2515247bfbfb
stipefrkovic <stipe.frkovic@tutanota.com>
2023-12-11 19:47:38 +0100

    added data and implemented db, created record model, tested simple query

---
+++ backend/models/record.ts
@@ -0,0 +1,47 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity({name: "records"})
+export class Record {
+    @PrimaryGeneratedColumn()
+    country!: string;
+    
+    @PrimaryGeneratedColumn()
+    year!: number;
+
+    @Column()
+    iso_code?: string;
+
+    @Column()
+    population?: number;
+
+    @Column()
+    co2?: number;
+
+    @Column()
+    energy_per_capita?: number;
+
+    @Column()
+    energy_per_gdp?: number;
+
+    @Column()
+    methane?: number;
+
+    @Column()
+    nitrous_oxide?: number;
+
+    @Column()
+    share_of_temperature_change_from_ghg?: number;
+
+    @Column()
+    temperature_change_from_ch4?: number;
+
+    @Column()
+    temperature_change_from_co2?: number;
+
+    @Column()
+    temperature_change_from_ghg?: number;
+
+    @Column()
+    temperature_change_from_n2o?: number;  
+
+}
--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,32 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm";
 import { config as dotenvConfig } from "dotenv";
+import Container from "typedi";
+
 import app from "./app";
+import { Record } from "./models/record";
+
 
 const PORT = process.env.PORT as unknown as number || 3000;
 const HOST = process.env.HOST || "localhost";
 
-app.listen(PORT, HOST, () => console.log(`Backend server listening on port ${PORT}`));
+
+const database = new DataSource({
+    type: "postgres",
+    host: process.env.DATABASE_HOST || "localhost",
+    port: process.env.DATABASE_PORT as unknown as number || 5432,
+    username: "postgres",
+    password: "postgres",
+    database: process.env.DATABASE_NAME,
+    entities: [
+        Record
+    ],
+    synchronize: false,
+    logging: false
+})
+
+database.initialize().then(database => {
+    Container.set<DataSource>("database", database);
+    app.listen(PORT, HOST, () => console.log(`Backend server listening on port ${PORT}`));
+}).catch(console.log)
+
