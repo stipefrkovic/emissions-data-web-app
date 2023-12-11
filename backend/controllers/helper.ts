494a9e2e7fa340c97b7dd9ffa7235c0a43a70ae6
Crenchi <christopherlambrinos@gmail.com>
2023-12-12 00:41:00 +0100

    tentative controls, missing models and db checks

--- backend/controllers/helper.ts
+++ backend/controllers/helper.ts
@@ -1,5 +1,6 @@
 import { ObjectLiteral, SelectQueryBuilder } from "typeorm";
 import { Min, Max, IsDefined } from "class-validator";
+import { Record} from "../models/record"
 
 /**
  * Interface that can be implemented by classes to allow different kinds of queries.
@@ -28,5 +29,26 @@ export class Paging<T extends ObjectLiteral> implements IQueryHelper<T> {
     if (!this.limit) this.limit = 100;
     if (!this.offset) this.offset = 0;
     return query.skip(this.offset).take(this.limit);
+  }
+}
+
+export class Order implements IQueryHelper<Record> {
+  "order-by": string;
+  "order": string;
+
+  public apply(query : SelectQueryBuilder<Record>) : SelectQueryBuilder<Record> {
+      if(!this["order-by"] || !this["order"]) return query;
+      return query.orderBy(this["order-by"], this["order"] === "desc" ? "DESC" : "ASC");
+  }
+}
+
+export class Filter implements IQueryHelper<Record> {
+  id?: string;
+  year?: number;
+
+  public apply(query : SelectQueryBuilder<Record>) : SelectQueryBuilder<Record> {
+      if(this.id) query = query.andWhere("record.id = :id", { id: this.id });
+      if(this.year) query = query.andWhere("record.year = :year", { year: this.year });
+      return query;
   }
 }
