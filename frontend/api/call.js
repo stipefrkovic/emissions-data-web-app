3b179c06f5b3b356b8b6746a1c996a488cef81b3
aerdelez <andro.erdelez@gmail.com>
2024-01-10 21:42:29 +0100

    again forgot to add

---
+++ frontend/api/call.js
@@ -0,0 +1,44 @@
+/**
+ * Makes an API call to the given URL
+ * @param url
+ * @param method
+ * @param data
+ * @returns a promise
+ */
+export default async function apiCall(url, method = "GET",  data = null)  {
+    // We obtain the server preference from the state storage (implemented using
+    // local storage APIs in this case)
+    const server = "http://localhost:3000";
+  
+    // Construct URL
+    url = `${server}/${url}`;
+  
+    // Set options
+    /** @type {RequestInit} */
+    const requestConfig = {
+        method: method,
+        headers: {
+            //TODO option menu csv
+            "Content-Type": "application/json",
+        },
+    };
+  
+    // Set data: in this case we want to send the request data as
+    // URL parameters for GET/HEAD/DELETE requests and only as request body
+    // for other requests. This is possible since our specification is compatible
+    // with this assumption. Other APIs might actually need both body and URL
+    // parameters.
+    if (data != null) {
+      if (method === "GET" || method === "HEAD" || method === "DELETE") {
+        // Use as parameter data
+        url += "?" + new URLSearchParams(data).toString();
+      } else {
+        // Use as body data
+        requestConfig.body = JSON.stringify(data);
+      }
+    }
+  
+    // Await the result: any errors will the thrown
+    return await fetch(url, requestConfig);
+  }
+  
