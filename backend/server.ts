49c61e78842562b3e6db8761f57d49d708ff28f6
Crenchi <christopherlambrinos@gmail.com>
2024-01-17 08:18:43 +0100

    Country + energy working

--- backend/server.ts
+++ backend/server.ts
@@ -1,3 +1,4 @@
+// import "reflect-metadata"
 import { DataSource } from "typeorm";
 import { config as dotenvConfig } from "dotenv";
 import Container from "typedi";
@@ -10,16 +11,16 @@ import { Country } from "./models/country";
 import { EnergyRecord } from "./models/energy-record";
 import { Continent } from "./models/continent";
 
-const BACKEND_PORT = process.env.BACKEND_PORT as unknown as number || 3000;
+const PORT = process.env.PORT as unknown as number || 3000;
 const HOST = process.env.HOST || "localhost";
-                                                                                                                                                                                                                                                                                                                                                                                                           
+
 const database = new DataSource({
     type: "mysql",
-    host: process.env.DATABASE_HOST || "localhost",
+    host: "mariadb",
     port: 3306,
-    username: process.env.DATABASE_USERNAME || "mariadb",
-    password: process.env.DATABASE_PASSWORD || "mariadb",
-    database: process.env.DATABASE_NAME || "emissions",
+    username: "mariadb",
+    password: "mariadb",
+    database: "emissions",
     entities: [
          GeneralRecord, EmissionRecord, EnergyRecord, Country, TemperatureRecord, Continent
     ],
@@ -29,8 +30,6 @@ const database = new DataSource({
 
 database.initialize().then(database => {
     Container.set<DataSource>("database", database);
-    console.log("Connected to database:")
-    console.log(database.options);
-    const server = app.listen(BACKEND_PORT, HOST, () => console.log(`Backend server started on port ${BACKEND_PORT}`));
-}).catch(console.error)
+    app.listen(PORT, HOST, () => console.log(`Backend server listening on port ${PORT}`));
+}).catch(console.log)
 
